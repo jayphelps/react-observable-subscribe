1f532cf5a0694d88f0a2e55163df5503a7d7d189
Jay Phelps <hello@jayphelps.com>
2016-05-04 11:49:28 -0700

    use startsWith

--- examples/basic/components/App.js
+++ examples/basic/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/startWith';
 import Example from './Example';
 
 export default
@@ -9,7 +10,7 @@ class App extends Component {
     // However you get your stream, e.g. redux or traditional state management
     // Can be any Observable that has observable[Symbol.observable]() which
     // is part of the TC39 observable spec for interop
-    const stream = Observable.interval(100);
+    const stream = Observable.interval(100).startWith('--');
     return (
       <div>
         <h1>Example of Observable streaming</h1>
